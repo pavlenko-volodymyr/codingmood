71fa314b00e88d1107d43a1dfcf9960e7a920916
SergeyKubrak <kartoswko@gmail.com>
2013-09-29 15:58:40 +0300

    Moved repo form to left side, add more style.

--- codemood/commits/forms.py
+++ codemood/commits/forms.py
@@ -5,15 +5,23 @@ from .models import Repository
 
 
 class RepositoryForm(forms.ModelForm):
-    start_date = forms.DateTimeField(required=False)
-    end_date = forms.DateTimeField(required=False)
+    start_date = forms.DateField(required=False, widget=forms.widgets.TextInput(attrs={'class': 'form-control input-sm',
+                                                                                'placeholder': 'DD/MM/YYYY'}))
+    end_date = forms.DateField(required=False, widget=forms.widgets.TextInput(attrs={'class': 'form-control input-sm',
+                                                                              'placeholder': 'DD/MM/YYYY'}))
 
     class Meta:
         model = Repository
         fields = ('url',)
+        widgets = {
+            'url': forms.widgets.TextInput(attrs={'class': 'form-control input-sm',
+                                                  'placeholder': ' https://github.com/user/reopsitory.git'}),
+        }
+
 
     def clean_url(self):
         data = self.cleaned_data['url']
+        print 1
         if not data.endswith('.git'):
             raise forms.ValidationError("Please give us url fo clone, for e.x. https://github.com/user/reopsitory.git")
         return data
