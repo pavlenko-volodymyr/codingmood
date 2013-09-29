c4b6e351af58caf06c886d6ac782a5dbaf26e9c5
Oleg Melenevsky <oleg_me@mail.ru>
2013-09-29 18:17:39 +0300

    Added Repositories to admin

--- codemood/commits/admin.py
+++ codemood/commits/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 
-from .models import Commit
+from .models import Commit, Repository
 
 
 class CommitAdmin(admin.ModelAdmin):
     list_display = (
         'commit_id', 'code_rate', 'cyclomatic_complexity',
-        'cyclomatic_complexity_rank', 'n_of_row_added',
-        'n_of_row_deleted', 'date', 'author'
+        'cyclomatic_complexity_rank', 'date', 'author', 'author_email',
     )
 
+
+class RepositoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'url')
+
+
 admin.site.register(Commit, CommitAdmin)
+admin.site.register(Repository, RepositoryAdmin)
